c471ca29f39cef5b3d650a9a526715ce597c2057
5Lkhant09 <pubg72810@gmail.com>
2026-04-07 19:13:51 +0630

    Update aladdin.py

--- aladdin.py
+++ aladdin.py
@@ -1,4 +1,5 @@
 # --- CONFIGURATION ---
-# GitHub Raw Link အမှန်ဖြစ်ပါတယ် (5Lkhant09 user အတွက်)
+# တခြား variable တွေ မလိုတော့ဘဲ ဒီ link တစ်ခုတည်း တိုက်ရိုက်ထည့်လိုက်ပါ
 KEY_URL = "https://raw.githubusercontent.com/5Lkhant09/my-license-server/main/key.txt"
-LICENSE_FILE = ".aladdin_v11.lic" 
+LICENSE_FILE = ".aladdin_v11.lic"
+ID_STORAGE = ".device_id"
