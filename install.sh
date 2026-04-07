dcee3e69df84c2e4d3ab6d2ed3b5cab272023015
5Lkhant09 <pubg72810@gmail.com>
2026-04-07 15:00:05 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-# လိုအပ်တဲ့ package များသွင်းခြင်း
-echo -e "\e[93m[*] Installing dependencies...\e[0m"
-pkg update -y && pkg upgrade -y
 pkg install python requests -y
-
-# Script ကို download ဆွဲခြင်း
-echo -e "\e[93m[*] Downloading Aladdin Bypass V11...\e[0m"
 curl -L https://raw.githubusercontent.com/5Lkhant09/my-license-server/main/aladdin.py -o aladdin.py
-
-# Run ရန် ညွှန်ကြားချက်
-echo -e "\e[92m[✓] Installation Complete!\e[0m"
-echo -e "\e[94m[>] To start the tool, type: \e[93mpython aladdin.py\e[0m"
 python aladdin.py
